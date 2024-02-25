9261ed83ed1ea776c7554c22dfafdd4e82d9dd34
Adrian Maj <adrianmaj1122@gmail.com>
2024-02-25 15:59:19 +0100

    Fixed shadows

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -42,6 +42,10 @@ const Navbar = () => {
 			<motion.nav
 				animate={{
 					top: isScrollingDown ? '-100%' : '0%',
+					boxShadow: isMenuOpen && isMobile ? 'none' : '0 4px 25px -25px rgb(0, 0, 0)',
+				}}
+				initial={{
+					boxShadow: '0 4px 25px -25px rgb(0, 0, 0)',
 				}}
 				className={classes.navbar}>
 				<Wrapper>
