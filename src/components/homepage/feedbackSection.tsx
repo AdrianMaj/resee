f7a95bed7805df610f5744f19ee06c7ba4535914
Adrian Maj <adrianmaj1122@gmail.com>
2024-02-26 16:18:52 +0100

    Added feedback section

--- src/components/homepage/feedbackSection.tsx
+++ src/components/homepage/feedbackSection.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Wrapper from '../ui/wrapper'
 import SectionHeading from '../ui/sectionHeading'
+import FeedbackSlider from '../sliders/feedbackSlider'
 
 const FeedbackSection = () => {
 	return (
 		<section className="section">
 			<Wrapper>
 				<SectionHeading>Feedback from our users</SectionHeading>
+				<FeedbackSlider />
 			</Wrapper>
 		</section>
 	)
