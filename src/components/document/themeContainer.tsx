57c4bce5a3cc31d0ace890163528fa3fbcf0ef3d
Adrian Maj <adrianmaj1122@gmail.com>
2024-03-06 18:36:50 +0100

    Fixed infinite loop

--- src/components/document/themeContainer.tsx
+++ src/components/document/themeContainer.tsx
@@ -8,13 +8,14 @@ import { Document, Page } from 'react-pdf'
 import _ from 'lodash'
 import { pdfjs } from 'react-pdf'
 import updatePDFUrl from '@/util/updatePDFUrl'
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
 const ThemeContainer = ({ documentData }: { documentData: UserDocument }) => {
 	const [instance] = usePDF({ document: <ThemeClassical documentData={documentData} /> })
 	const [uploadedFile, setUploadedFile] = useState({ URL: '', publicId: '' })
 	const [previousUploadedFile, setPreviousUploadedFile] = useState({ URL: '', publicId: '' })
-	pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 	useEffect(() => {
+		let isMounted = true
 		const handleInstanceChange = async () => {
 			if (instance.blob) {
 				const file = new File([instance.blob], `${documentData.name}.pdf`, {
@@ -23,18 +24,25 @@ const ThemeContainer = ({ documentData }: { documentData: UserDocument }) => {
 				const formData = new FormData()
 				formData.append('file', file)
 				formData.append('upload_preset', 'reseeFiles')
+
 				try {
 					const response = await fetch(`https://api.cloudinary.com/v1_1/dcl15uhh0/raw/upload`, {
 						method: 'POST',
 						body: formData,
 					})
 					const res = await response.json()
-					setPreviousUploadedFile(uploadedFile)
-					setUploadedFile({
-						URL: res.secure_url,
-						publicId: res.public_id,
-					})
-					updatePDFUrl(documentData.id, res.secure_url, res.public_id)
+
+					if (isMounted) {
+						setPreviousUploadedFile(uploadedFile)
+						if (previousUploadedFile.publicId) {
+							await updatePDFUrl(documentData.id, res.secure_url, previousUploadedFile.publicId)
+						}
+
+						setUploadedFile({
+							URL: res.secure_url,
+							publicId: res.public_id,
+						})
+					}
 				} catch (error) {
 					console.error(error)
 				}
@@ -42,8 +50,14 @@ const ThemeContainer = ({ documentData }: { documentData: UserDocument }) => {
 		}
 
 		const debouncedHandleInstanceChange = _.debounce(handleInstanceChange, 1000)
+
 		debouncedHandleInstanceChange()
-	}, [instance.blob, uploadedFile, previousUploadedFile.URL, documentData.id, documentData.name])
+
+		return () => {
+			isMounted = false
+		}
+	}, [documentData.id, instance.blob, documentData.name, uploadedFile, previousUploadedFile.publicId])
+
 	return (
 		<section className={classes.container}>
 			<a href={uploadedFile.URL}>Test URL</a>
