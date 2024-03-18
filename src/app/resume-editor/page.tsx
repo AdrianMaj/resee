69ca1fec05c704eae1f124e7968e1d3606211247
Adrian Maj <adrianmaj1122@gmail.com>
2024-03-18 17:56:13 +0100

    Fixed login bug

--- src/app/resume-editor/page.tsx
+++ src/app/resume-editor/page.tsx
@@ -2,15 +2,17 @@ import Navbar from '@/components/navbar/navbar'
 import EditorSection from '@/components/resume-editor/editorSection'
 import FooterSection from '@/components/ui/footerSection'
 import fetchAccountWithDocuments from '@/util/fetchAccountWithDocuments'
+import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
 const Page = async () => {
 	const account = await fetchAccountWithDocuments()
+	const session = await getServerSession()
 	if (account) {
 		return (
 			<>
-				<Navbar userAccount={account}></Navbar>
+				<Navbar session={session}></Navbar>
 				<EditorSection documents={account.UserDocument} userId={account.id} />
 				<FooterSection />
 			</>
